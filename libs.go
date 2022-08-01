86a0d8bf7b7cf09e243680de08f642286bb2bada
Nabeel Saabna <15704531+nabeelys@users.noreply.github.com>
2022-08-01 12:49:51 +0300

    Update libs.go

--- libs.go
+++ libs.go
@@ -1,9 +1,17 @@
 package libs
 
 func PrintMultiply(a, b int) int {
-  fmt.Println(a * b)
+  fmt.Print(a * b)
 }
 
 func PrintSum(a, b int) int {
+  fmt.Print(a + b)
+}
+
+func PrintMultiplyLn(a, b int) int {
+  fmt.Println(a * b)
+}
+
+func PrintSumLn(a, b int) int {
   fmt.Println(a + b)
 }
